06bf2b7c63e5c1c78fbed719ed42376ec2f510f4
Nolan <nolan.erck@gmail.com>
2022-01-27 17:55:05 -0800

    Updates to the Docker SQL setup.

--- _db/000_vandelayindustries.sql
+++ _db/000_vandelayindustries.sql
@@ -7,10 +7,13 @@ USE vandelayindustries;
 
 DROP TABLE IF EXISTS `tCustomers`;
 CREATE TABLE `tCustomers` (
-  `CustomerPK` int(11) NOT NULL,
+  `CustomerPK` int(11) NOT NULL AUTO_INCREMENT,
   `FirstName` VARCHAR(50) DEFAULT NULL,
   `LastName` VARCHAR(50) DEFAULT NULL,
   PRIMARY KEY (`CustomerPK`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO tCustomers( FirstName, LastName ) VALUES( 'John', 'Lennon' )
+INSERT INTO tCustomers( FirstName, LastName ) VALUES( 'John', 'Lennon' );
+INSERT INTO tCustomers( FirstName, LastName ) VALUES( 'Paul', 'McCartney' );
+INSERT INTO tCustomers( FirstName, LastName ) VALUES( 'George', 'Harrison' );
+INSERT INTO tCustomers( FirstName, LastName ) VALUES( 'Ringo', 'Starr' );
